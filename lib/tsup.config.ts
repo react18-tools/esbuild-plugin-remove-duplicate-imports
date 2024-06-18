9d22bc8d24a0be472b95f22b054e04fa1a339d2b
Mayank <mayank.srmu@gmail.com>
2024-06-18 20:27:53 +0530

    Update tsup config

--- lib/tsup.config.ts
+++ lib/tsup.config.ts
@@ -1,18 +1,17 @@
 import { defineConfig, type Options } from "tsup";
-import react18Plugin from "esbuild-plugin-react18";
-import cssPlugin from "esbuild-plugin-react18-css";
 
 export default defineConfig(
   (options: Options) =>
     ({
       format: ["cjs", "esm"],
       target: "es2019",
-      entry: ["./src"],
+      entry: ["./src/index.ts"],
       sourcemap: false,
       clean: !options.watch,
-      bundle: true,
       minify: !options.watch,
-      esbuildPlugins: [react18Plugin(), cssPlugin({ generateScopedName: "[folder]__[local]" })],
+      bundle: true,
+      dts: true,
+      footer: { js: "module.exports = module.exports.default;" },
       ...options,
     }) as Options,
 );
