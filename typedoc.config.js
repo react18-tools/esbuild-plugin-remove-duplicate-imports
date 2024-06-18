81cc54da8a1f0d909db4471640b40904914b90d8
Mayank <mayank.srmu@gmail.com>
2024-06-18 20:24:38 +0530

    update docs config

--- typedoc.config.js
+++ typedoc.config.js
@@ -3,7 +3,7 @@ module.exports = {
   tsconfig: "tsconfig.docs.json",
   name: "Esbuild Plugin Rdi",
   entryPoints: ["./lib/src"],
-  exclude: ["**/*.test.tsx", "**/index.ts", "**/declaration.d.ts"],
+  exclude: ["**/*.test.tsx", "**/declaration.d.ts"],
   entryPointStrategy: "Expand",
   out: "./docs",
   commentStyle: "all",
